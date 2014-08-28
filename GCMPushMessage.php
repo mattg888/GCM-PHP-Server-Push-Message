5e5b6c79626cd6b1b2dcb827216113a57d40b163
Franklin Waller <franklin.dijks@gmail.com>
2014-08-28 17:51:54 +0200

    Use __construct instead of same classname.

--- GCMPushMessage.php
+++ GCMPushMessage.php
@@ -19,17 +19,21 @@
 	http://stackoverflow.com/questions/11242743/gcm-with-php-google-cloud-messaging
 
 */
+
+namespace Google\GCM;
+
 class GCMPushMessage {
 
 	var $url = 'https://android.googleapis.com/gcm/send';
 	var $serverApiKey = "";
 	var $devices = array();
-	
+
 	/*
 		Constructor
 		@param $apiKeyIn the server API key
-	*/
-	function GCMPushMessage($apiKeyIn){
+	*/             
+        
+	public function __construct($apiKeyIn){
 		$this->serverApiKey = $apiKeyIn;
 	}
 
@@ -38,13 +42,13 @@ class GCMPushMessage {
 		@param $deviceIds array of device tokens to send to
 	*/
 	function setDevices($deviceIds){
-	
+
 		if(is_array($deviceIds)){
 			$this->devices = $deviceIds;
 		} else {
 			$this->devices = array($deviceIds);
 		}
-	
+
 	}
 
 	/*
@@ -53,20 +57,20 @@ class GCMPushMessage {
 		@param $data Array of data to accompany the message
 	*/
 	function send($message, $data = false){
-		
+
 		if(!is_array($this->devices) || count($this->devices) == 0){
 			$this->error("No devices set");
 		}
-		
+
 		if(strlen($this->serverApiKey) < 8){
 			$this->error("Server API Key not set");
 		}
-		
+
 		$fields = array(
 			'registration_ids'  => $this->devices,
 			'data'              => array( "message" => $message ),
 		);
-		
+
 		if(is_array($data)){
 			foreach ($data as $key => $value) {
 				$fields['data'][$key] = $value;
@@ -80,25 +84,25 @@ class GCMPushMessage {
 
 		// Open connection
 		$ch = curl_init();
-		
+
 		// Set the url, number of POST vars, POST data
 		curl_setopt( $ch, CURLOPT_URL, $this->url );
-		
+
 		curl_setopt( $ch, CURLOPT_POST, true );
 		curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-		
+
 		curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $fields ) );
-		
+
 		// Execute post
 		$result = curl_exec($ch);
-		
+
 		// Close connection
 		curl_close($ch);
-		
+
 		return $result;
 	}
-	
+
 	function error($msg){
 		echo "Android send notification failed with error:";
 		echo "\t" . $msg;
